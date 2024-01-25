0a71569acc36440545329a1d20f29739bd137b41
Sinless Rook <142495840+SinlessRook@users.noreply.github.com>
2024-01-25 13:05:29 +0530

    second commit

--- app.py
+++ app.py
@@ -9,32 +9,53 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return "No file part"
+# @app.route('/', methods=['POST'])
+# def upload():
+#     if 'file' not in request.files:
+#         return "No file part"
     
-    file = request.files['file']
+#     file = request.files['file']
     
-    if file.filename == '':
-        return "No selected file"
+#     if file.filename == '':
+#         return "No selected file"
     
-    if file:
-        # Perform necessary operations on the image (resizing in this case)
-        img = Image.open(file)
-        img = img.rotate(90)  # Resize the image to 300x300 pixels
+#     if file:
+#         # Perform necessary operations on the image (resizing in this case)
+#         img = Image.open(file)
+#         img = img.rotate(90)  # Resize the image to 300x300 pixels
         
-        # Save the modified image to a BytesIO object
-        output_buffer = BytesIO()
-        img.save(output_buffer, format='PNG')
-        output_buffer.seek(0)
+#         # Save the modified image to a BytesIO object
+#         output_buffer = BytesIO()
+#         img.save(output_buffer, format='PNG')
+#         output_buffer.seek(0)
         
-        # Convert the BytesIO object to a base64-encoded string
-        output_data = output_buffer.getvalue()
-        output_base64 = base64.b64encode(output_data).decode('utf-8')
+#         # Convert the BytesIO object to a base64-encoded string
+#         output_data = output_buffer.getvalue()
+#         output_base64 = base64.b64encode(output_data).decode('utf-8')
         
-        # Return the modified image data as a base64-encoded string
-        return render_template('index.html', img_data=output_base64)
+#         # Return the modified image data as a base64-encoded string
+#         return render_template('index.html', img_data=output_base64)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/pdf')
+def pdf():
+    return render_template('pdf.html')
+
+@app.route('/extract')
+def extract():
+    return render_template('extract.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+@app.route('/format')
+def format():
+    return render_template('format.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
