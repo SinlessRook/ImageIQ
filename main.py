aa99b94bfad8bdc040ef8c2b1b7e91a7998d8e8e
Sinless Rook <142495840+SinlessRook@users.noreply.github.com>
2024-01-26 14:31:39 +0530

    fifth commit

--- main.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-import os
-img=Image.open(r"C:\Users\Admin\Downloads\ImageIQ2\ImageIQ\static\assets\pdf.jpg")
-img.convert("RGB").save(r"C:\Users\Admin\Downloads\ImageIQ2\ImageIQ\static\assets\pdf.png")
-os.remove(r"C:\Users\Admin\Downloads\ImageIQ2\ImageIQ\static\assets\pdf.jpg")
